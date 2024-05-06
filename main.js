347aa14f130ba5d83e5b12e0def21f8e059c33e3
muhammad nasir <muhammadnasir8787914@gmail.com>
2024-05-06 23:58:12 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([{
+            name: "todoAdd",
+            type: "input",
+            message: "what would you like to add todo?"
+        },
+        {
+            name: "todoMoreadd",
+            type: "confirm",
+            message: "would you like to add more in your todo list?",
+            default: "true"
+        },
+    ]);
+    if (todoQuestions.todoAdd.length >= 2) {
+        todos.push(todoQuestions.todoAdd);
+    }
+    else {
+        condition = false;
+    }
+    console.log(todos);
+}
